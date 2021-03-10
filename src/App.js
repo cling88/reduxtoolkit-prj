17a928d71ab7ed0dd84a5b3418d83789465a0df1
cling88 <cling88@tlnine.co.kr>
2021-03-10 11:06:01 +0900

    basic tutorial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+/*
+First Write : 2021.03.10
+https://redux-toolkit.js.org/tutorials/quick-start
+yarn add react-redux @reduxjs/toolkit
+*/
 
+import Counter from './pages/Counter'
 
 function App() {
   return (
     <div className="App">
-      Basic
+      <Counter/>
     </div>
   );
 }
